5fd67db319258fb62023e79f42a8eb57fc947225
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2021-11-12 19:36:40 -0500

    Lesson 2 Rotations and firing projectile

--- EricQGeekeduProject/Assets/PlayerScript.cs
+++ EricQGeekeduProject/Assets/PlayerScript.cs
@@ -8,9 +8,24 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody rb; // this is a reference to our rigidbody
     public int jumpForce = 50; // this is the jump force
     public bool isJumping; // a true or false if we're jumping
+
+    public float horizontalSpeed; // how fast we rotate left and right
+    public float verticalSpeed; // how fast we rotate up and down
+
+    public float xRotation; // holding our current x rotation
+    public float yRotation; // holding our current y rotation
+
+    public Vector3 MoveDirection; // new forward position
+
+    // Bullet stuff
+    public GameObject Bullet; // a prefab of our bullet
+    public float BulletForce; // how fast the bullet will travel
+    public Transform ShootingPosition; // where the bullet comes from
+
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked; // freezes our mouse position to the center of the screen
         rb = GetComponent<Rigidbody>();
         
     }
@@ -19,30 +34,65 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         Move();
+        PlayerRotate();
+        Shoot();
     }
 
     void Move() // our movement inputs
     {
-        if (Input.GetKey("w")) // moving forward
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("s")) // moving backward
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("a")) // moving left
-        {
-            transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
-        }
-        if (Input.GetKey("d")) // moving right
-        {
-            transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
-        }
+        //if (Input.GetKey("w")) // moving forward
+        //{
+        //    //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed * Time.deltaTime);
+        //    transform.Translate(transform.forward * (moveSpeed * Time.deltaTime));
+        //}
+        //if (Input.GetKey("s")) // moving backward
+        //{
+        //    //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveSpeed * Time.deltaTime);
+        //    transform.Translate(-transform.forward * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey("a")) // moving left
+        //{
+        //    //transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        //    transform.Translate(-transform.right * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKey("d")) // moving right
+        //{
+        //    //transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        //    transform.Translate(transform.right * moveSpeed * Time.deltaTime);
+        //}
+
+        float horizontal = Input.GetAxis("Horizontal"); // store our horizontal input
+        float vertical = Input.GetAxis("Vertical"); // store our vertical input
+        MoveDirection = transform.forward * vertical + transform.right * horizontal; // apply those to our move direction
+        Vector3 force = MoveDirection * (moveSpeed * Time.deltaTime); // create a force using move speed and our new direction
+        transform.position += force; // apply all of that to our position
+
         if (Input.GetKeyDown("space") && isJumping == false) // jumping
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // 0, 1, 0
             isJumping = true;
+        }
+    }
+
+    void PlayerRotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X"); // holds our x mouse position
+        yRotation = mouseX * horizontalSpeed * Time.deltaTime; // our new y rotation
+        float mouseY = Input.GetAxis("Mouse Y"); // holds our new y mouse position
+        xRotation = mouseY * verticalSpeed * Time.deltaTime; // our new x rotation
+
+        Vector3 PlayerRotation = transform.rotation.eulerAngles; // making a local variable to hold our current rotation
+        PlayerRotation.y += yRotation; // setting our y rotation
+        PlayerRotation.x -= xRotation; // setting our x rotation
+        transform.rotation = Quaternion.Euler(PlayerRotation); // apply the rotation to our character
+    }
+
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0)) // when we left click
+        {
+            GameObject newBullet = Instantiate(Bullet, ShootingPosition.position, transform.rotation); // make a new bullet at the bullet shoot position with players rotation
+            newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * BulletForce); // give force to bullet
         }
     }
 
